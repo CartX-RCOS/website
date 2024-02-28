002cd931b1a6e4e0c8bd532e5819af22c4ee79fc
Robert Acebedo <robace747@gmail.com>
2024-02-28 15:30:03 -0500

    Display Address
    fixed css and get address using browser

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import SearchBar from '../SearchBar/SearchBar'
 import AddressPopup from '../AddressPopup/AddressPopup';
-import img from '../../assets/CartX-logos/logo-transparent-png.png'
-import React, { useState } from 'react';
+import img from '../../assets/CartX-logos/Updated Logos/logo-transparent-png.png'
+import { useState } from 'react';
+import {TiLocation} from "react-icons/ti";
 import './Navbar.css'
 
-const NavBar = () => {
+const NavBar = ({ address, changeAddress}) => {
   const [showPopup, setShowPopup] = useState(false);
   
   const handleButtonClick = () => {
@@ -18,24 +19,34 @@ const NavBar = () => {
   return (
    <>
     <div className="navbar">
-
         <div className="left">
-        <label className="burger" for="burger">
-          <input type="checkbox" id="burger"/>
-          <span></span>
-          <span></span>
-          <span></span>
-        </label>
-          <img className="logo" src={ img } alt="CartX Logo" width="60"height="60"/>
+          <div className="burger_div">
+            <label className="burger" for="burger">
+              <input type="checkbox" id="burger"/>
+              <span></span>
+              <span></span>
+              <span></span>
+            </label>
+          </div>
+          <div className="logo_div">
+            <img className="logo" src={ img } alt="CartX Logo" width="140"/>
+          </div>
         </div>
 
         <div className="middle">
-          <SearchBar/>
+          <SearchBar className="search"/>
         </div>
 
         <div className="right">
-          <button onClick={handleButtonClick}>Open Address Popup</button>
-          {showPopup && <AddressPopup onClose={handleClosePopup} />}
+          <div className="address_div" onClick={handleButtonClick}>
+            <button id="address_button">
+              <TiLocation size={36} />
+            </button>
+            {showPopup && <AddressPopup onClose={handleClosePopup}/>}
+            <div className="text_div">
+              {localStorage.getItem('address')}
+            </div>
+          </div>
         </div>
     </div>    
    </>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,32 @@
+import { useState, useEffect } from 'react';
 import './index.css'
 import NavBar from '../../components/Navbar/Navbar'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import Content from '../../components/Content/Content'
 
 const Home = () => {
+  const [address, setAddress] = useState();
+
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        let latitude = position.coords.latitude;
+        let longitude = position.coords.longitude;
+        setAddress(latitude);
+      });
+    }
+    else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  useEffect(() => {
+    if (address){
+      localStorage.setItem('address', address);
+    }
+  }, [address]);
+
   return (
    <>
       <Sidebar />  
