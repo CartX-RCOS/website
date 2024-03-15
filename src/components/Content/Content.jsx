22974c07754931d12f1e21ce807577e028034cf3
Robert Acebedo <robace747@gmail.com>
2024-03-15 17:03:12 -0400

    card display

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,9 +1,24 @@
+import React from 'react';
+import Card from '../Card/Card';
 import './Content.css'
 
 const Content = ({ sidebar }) => {
   return (
    <>
-      <div className="content" style={!sidebar ? { width: "100vw", left: "0%" } : null }>Content</div>   
+    <div className="content-wrapper">
+    <div className="content" style={!sidebar ? { width: "100vw", left: "0%" } : null }>
+        
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        
+        </div>   
+    </div>
    </>
   )
 }
