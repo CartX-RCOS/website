e40f710c7b5f2933e55696a54ccc2722db08c7cb
Robert Acebedo <robace747@gmail.com>
2024-03-24 16:31:48 -0400

    display data

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import './Card.css';
 import {MDBCard, MDBCardBody, MDBCardImage, MDBRipple } from "mdb-react-ui-kit";
 
-const Card = () => {
-
+const Card = ({ data }) => {
+  console.log(data.images_links?.[0]?.product_image_url ?? 'Default URL');
    return (
       <>
          <MDBCard>
@@ -13,18 +13,11 @@ const Card = () => {
               className="bg-image rounded hover-zoom"
             >
               <MDBCardImage
-                src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/belt.webp"
+                src={data.images_links?.[0]?.product_image_url ?? ""} 
                 fluid
                 className="w-100"
               />
               <a href="#!">
-                <div className="mask">
-                  <div className="d-flex justify-content-start align-items-end h-100">
-                    <h5>
-                      <span className="badge bg-primary ms-2">New</span>
-                    </h5>
-                  </div>
-                </div>
                 <div className="hover-overlay">
                   <div
                     className="mask"
@@ -35,12 +28,11 @@ const Card = () => {
             </MDBRipple>
             <MDBCardBody>
               <a href="#!" className="text-reset">
-                <h5 className="card-title mb-3">Product name</h5>
+                <h5 className="card-title mb-3">{data.name}</h5>
               </a>
               <a href="#!" className="text-reset">
-                <p>Category</p>
+                {/* <p>{data.size}</p> */}
               </a>
-              <h6 className="mb-3">$61.99</h6>
             </MDBCardBody>
          </MDBCard>
       </>
