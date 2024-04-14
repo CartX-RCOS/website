e7cef08b210d8f6b7581c5b951892664467de733
Vitisadi <82157788+Vitisadi@users.noreply.github.com>
2024-04-13 23:13:02 -0500

    Added a store card to display stores in a cleaner fashion

--- src/pages/Analyze/index.jsx
+++ src/pages/Analyze/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import AnalysisCard from '../../components/AnalysisCard/AnalysisCard';
+import StoreCard from '../../components/StoreCard/StoreCard';
 
 const AnalyzeStores = () => {
    const [stores, setStores] = useState([]);
@@ -18,11 +19,12 @@ const AnalyzeStores = () => {
       <div className="store-analysis">
          {stores.map((store, index) => (
          <div key={index} className="store">
-            <h2>{`${index + 1}. ${store.name}`}</h2>
+            <StoreCard key={index} store={store} index={index} />
+            {/* <h2>{`${index + 1}. ${store.name}`}</h2>
             <p>Address: {store.address}</p>
             <p>Distance: {store.distance.toFixed(2)} miles</p>
             <p>Travel Time: {store.travelTime.toFixed(2)} minutes</p>
-            <p>Analysis Points: {store.analysisPoints.toFixed(2)}</p>
+            <p>Analysis Points: {store.analysisPoints.toFixed(2)}</p> */}
             <div className="matched-items">
                <h3>Matched Items:</h3>
                <div className="store-analysis">
